68c45265050fbaab82507087aa44e3c40e7b9cac
Michael Renken <michaelarenken@gmail.com>
2018-07-06 12:11:53 -0500

    Added survey models

--- survey/models.py
+++ survey/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class SurveyQuestion(models.Model):
+    question = models.TextField()
+
+    def __str__(self):
+        return self.question
+
+class SurveyAnswer(models.Model):
+    question = models.ForeignKey(
+        'SurveyQuestion',
+        related_name='answers',
+        on_delete=models.CASCADE,
+    )
+    answer = models.TextField()
+
+class SurveyResponse(models.Model):
+    ip = models.GenericIPAddressField()
+    question = models.ForeignKey(
+        'SurveyQuestion',
+        related_name='responses',
+        on_delete=models.CASCADE,
+    )
+    answer = models.ForeignKey(
+        'SurveyAnswer',
+        related_name='responses',
+        on_delete=models.CASCADE,
+    )
