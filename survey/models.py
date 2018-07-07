a73a56b100e574d8e052d75bf2e6dde0ba7d6414
Michael Renken <michaelarenken@gmail.com>
2018-07-07 10:37:42 -0500

    Added the random classmethod to SurveyQuestion

--- survey/models.py
+++ survey/models.py
@@ -1,3 +1,5 @@
+import random
+
 from django.db import models
 
 class SurveyQuestion(models.Model):
@@ -6,6 +8,19 @@ class SurveyQuestion(models.Model):
     def __str__(self):
         return self.question
 
+    @classmethod
+    def random(cls):
+        """
+            get a random entry
+        """
+        max_pk = cls.objects.values_list('pk', flat=True).order_by('-pk')[0]
+        while True:
+            try:
+                return cls.objects.get(pk=random.randint(1, max_pk))
+
+            except cls.DoesNotExist:
+                pass
+
 class SurveyAnswer(models.Model):
     question = models.ForeignKey(
         'SurveyQuestion',
@@ -13,6 +28,9 @@ class SurveyAnswer(models.Model):
         on_delete=models.CASCADE,
     )
     answer = models.TextField()
+
+    def __str__(self):
+        return self.answer
 
 class SurveyResponse(models.Model):
     ip = models.GenericIPAddressField()
--- survey/tests.py
+++ survey/tests.py
@@ -1,3 +1,26 @@
+from unittest import mock
+
 from django.test import TestCase
 
-# Create your tests here.
+from .models import SurveyQuestion
+
+class TestSurveyQuestion(TestCase):
+
+    def test_random(self):
+        q1 = SurveyQuestion.objects.create(question='Q1')
+        q1.answers.create(answer='Y')
+        q1.answers.create(answer='N')
+
+        self.assertEqual(SurveyQuestion.random(), q1)
+
+        q2 = SurveyQuestion.objects.create(question='Q2')
+        q2.answers.create(answer='Y2')
+        q2.answers.create(answer='N2')
+
+        # Delete the first question so we can get a key mismatch on random.
+        SurveyQuestion.objects.get(pk=1).delete()
+
+        # Try the deleted entry then the existing entry. This tests the
+        # DoesNotExist branch.
+        with mock.patch('random.randint', side_effect=[1, 2]):
+            self.assertEqual(SurveyQuestion.random(), q2)
